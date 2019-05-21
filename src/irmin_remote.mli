4ee9dfbff09c7b6681a37d100cfcba6b0baa12d5
Zach Shipko <zachshipko@gmail.com>
2019-05-20 18:42:18 -0700

    Add some comments

--- src/irmin_remote.mli
+++ src/irmin_remote.mli
@@ -1,34 +1,51 @@
+(** [STORAGE] is used to define a remote storage implementation *)
 module type STORAGE = sig
+  (** Storage context *)
   type t
 
+  (** Context serializer *)
   val t : t Irmin.Type.t
 
+  (** Irmin configuration key *)
   val config_key : t option Irmin.Private.Conf.key
 
+  (** [true] when the key exists, otherwise [false] *)
   val exists : t -> string -> bool Lwt.t
 
+  (** Get the value associated with the given key,
+      [None] will be returned when the key is empty. *)
   val get : t -> string -> string option Lwt.t
 
+  (** [put ctx key value] sets [key] to [value] *)
   val put : t -> string -> string -> unit Lwt.t
 
+  (** Removes the given key from the remote store *)
   val del : t -> string -> unit Lwt.t
 end
 
+(** [Content_addressable] creates an [Irmin.CONTENT_ADDRESSABLE_STORE_MAKER]
+    for the given remote storage implementation *)
 module Content_addressable (Storage : STORAGE) :
   Irmin.CONTENT_ADDRESSABLE_STORE_MAKER
 
+(** [Make] creates an [Irmin.S_MAKER] for the given remote storage and
+    [Irmin.ATOMIC_WRITE_STORE_MAKER] implementations *)
 module Make (Storage : STORAGE) (AW : Irmin.ATOMIC_WRITE_STORE_MAKER) :
   Irmin.S_MAKER
 
+(** [KV] creates an [Irmin.KV_MAKER] for the given remote storage and
+    [Irmin.ATOMIC_WRITE_STORE_MAKER] implementations *)
 module KV (Storage : STORAGE) (AW : Irmin.ATOMIC_WRITE_STORE_MAKER) :
   Irmin.KV_MAKER
 
+(** Convenience wrappers for creating in-memory stores *)
 module Mem : sig
   module Make (Storage : STORAGE) : Irmin.S_MAKER
 
   module KV (Storage : STORAGE) : Irmin.KV_MAKER
 end
 
+(** Convenience wrappers for creating Git stores *)
 module Git : sig
   module Make
       (Storage : STORAGE)
